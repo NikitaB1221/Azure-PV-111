7df56655cb4497b323d581201743ce326ca0cca8
Bodu_wk69 <Bodu_wk69@student.itstep.org>
2023-11-29 19:57:48 +0200

    Automatization

--- Azure-PV-111/wwwroot/js/site.js
+++ Azure-PV-111/wwwroot/js/site.js
@@ -13,7 +13,60 @@
     if (transliterateButton) {
         transliterateButton.addEventListener('click', transliterateClick);
     }
+
+    if (translateButton) {
+        document.addEventListener("selectionchange", onSelectionChange);
+    }
 });
+
+
+let lastSelectionTimestamp = 0;
+let delayedAction;
+function onSelectionChange(e) {
+    if (e.timeStamp - lastSelectionTimestamp < 300) {
+        if (typeof delayedAction != 'undefined') {
+            clearTimeout(delayedAction);
+        }
+    }
+    lastSelectionTimestamp = e.timeStamp;
+    delayedAction = setTimeout(translateSelection,1000);
+    //console.log(e);
+    //console.log(document.getSelection().toString());
+}
+
+function translateSelection() {
+    const check = document.getElementById("translator-selection");
+    const check2 = document.getElementById("transliterator-selection");
+    let text = document.getSelection().toString().trim();
+    let [_, lang_from, lang_to, output] = getTranslateData();
+    if (text.length > 0 && check.checked) {
+        fetch(`/api/translate?text=${text}&from=${lang_from.value}&to=${lang_to.value}`)
+            .then(r => r.json())
+            .then(j => {
+                console.log(j);
+                output.value = j[0].translations[0].text;
+            });
+    }
+    else if (check2.checked) {
+        const fromScript = lang_from.selectedOptions[0].getAttribute("data-script");
+        if (!fromScript) {
+            alert("Транслітерація цієї мови не підтримується");
+            return;
+        }
+        const toScript = "Latn";
+        fetch(
+            `/api/translate?text=${text}&from=${lang_from.value}&fromScript=${fromScript}&toScript=${toScript}`, {
+            method: "POST"
+        })
+            .then(r => r.json())
+            .then(j => {
+                console.log(j);
+                alert(`${text} ==> ${j[0].text}`);
+            });
+    }
+    delayedAction = undefined;
+
+}
 
 function getTranslateData() {
     const input = document.getElementById("translator-input");
@@ -52,18 +105,21 @@ function switchClick() {
     console.log(" | 1I.V " + input.value + " | 1O.V " + output.value);
     console.log(" | 1LF.IT " + lang_from.selectedOptions[0].innerText + " | LT.IT " + lang_to.selectedOptions[0].innerText);
 
-    let tmp_lang_val = lang_to;
-    lang_to = lang_from;
-    lang_from = tmp_lang_val;
+    //let tmp_lang_val = lang_to.selectedOptions[0].value;
+    //lang_to.selectedOptions[0].value = lang_from.selectedOptions[0].value;
+    //lang_from.selectedOptions[0].value = tmp_lang_val;
 
+    let tmp_lang_val = lang_to.value;
+    lang_to.value = lang_from.value;
+    lang_from.value = tmp_lang_val;
 
+    //tmp_lang_val = lang_to.selectedOptions[0].innerText;
+    //lang_to.selectedOptions[0].innerText = lang_from.selectedOptions[0].innerText;
+    //lang_from.selectedOptions[0].innerText = tmp_lang_val;
 
-    tmp_lang_val = lang_to.selectedOptions[0].innerText;
-    lang_to.selectedOptions[0].innerText = lang_from.selectedOptions[0].innerText;
-    lang_from.selectedOptions[0].innerText = tmp_lang_val;
-
+    let tmp_txt = input.value;
     input.value = output.value;
-    output.value = "";
+    output.value = tmp_txt;
 
     text = input.value;
 
@@ -80,21 +136,36 @@ function switchClick() {
 }
 
 function translateClick() {
+    let [text, langFrom, langTo, output] = getTranslateData();
+    fetch(`/api/translate?text=${text}&from=${langFrom.value}&to=${langTo.value}`)
+        .then(r => r.json())
+        .then(j => {
+            const data = j[0];
+            console.log(j);
+            if (typeof data['detectedLanguage'] !== 'undefined') {
+                langFrom.value = data['detectedLanguage']['language'];
 
-    let [text, lang_from, lang_to, output] = getTranslateData();
-    //output.value = input.value + ' ' + lang_from.value + ' ' + lang_to.value;
+                const lang = data['detectedLanguage']['language'];
 
-    if (text.length == 0) {
-        alert("Введите текст для перевода");
-        input.focus();
-        return;
-    }
+                let opts = langFrom.querySelectorAll(`option[disabled]`);
+                for (let op of opts) {
+                    langFrom.removeChild(op);
+                }
+                let opt = document.createElement('option');
+                opt.disabled = true;
+                opt.selected = true;
+                opt.value = lang;
+                opt.innerText = langFrom.querySelector(`option[value=${lang}]`)
+                    .innerText + ' (detected)';
+                langFrom.options.add(opt);
+            }
+            output.value = data.translations[0].text;
+        });
 
-    fetch(`/api/translate?text=${text}&from=${lang_from.value}&to=${lang_to.value}`).then(r => r.json()).then(j => {
-        console.log(j);
-        output.value = j[0].translations[0].text;
-    });
+    // output.value = input.value + ' ' + langFrom.value + ' ' + langTo.value;
+    // "translator-switch" "translator-lang-from" "translator-lang-to"
 }
+
 function transliterateClick() {
 
     let [_, __, lang_to, output] = getTranslateData();
